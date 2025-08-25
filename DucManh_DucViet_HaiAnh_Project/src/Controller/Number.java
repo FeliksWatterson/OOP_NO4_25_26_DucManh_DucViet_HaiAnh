20b5d45285d3e22a8f34e0afacd84590e4b9a527
Lê Hoàng Đức Mạnh <94680558+FeliksWatterson@users.noreply.github.com>
2025-08-25 10:08:15 +0000

    GDT

--- DucManh_DucViet_HaiAnh_Project/src/Controller/Number.java
+++ DucManh_DucViet_HaiAnh_Project/src/Controller/Number.java
@@ -10,11 +10,11 @@ public class Number {
         n1.i = 2;
         n2.i = 5;
 
-        n1 = n2;        // từ đây n1 và n2 cùng trỏ tới 1 object
+        n1 = n2;    
         n2.i = 10;
-        System.out.println(n2.i); // in ra 10
+        System.out.println(n2.i); 
 
         n1.i = 20;
-        System.out.println(n1.i); // in ra 20
+        System.out.println(n1.i); 
     }
 }
