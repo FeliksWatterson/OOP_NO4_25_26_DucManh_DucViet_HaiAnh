2a0dba43d22d838010872657fd4fffa915b0b47b
HaianhSp <haianhle399@gmail.com>
2025-08-25 16:33:35 +0700

    n1 n2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
-public class Main {
+class Number {   
+    public int i;
+}
+
+public class Main {  
     public static void main(String[] args) {
-        System.out.println("Game Started!");
+        Number n1 = new Number();
+        Number n2 = new Number();
+
+        n1.i = 2;
+        n2.i = 5;
+
+        n1 = n2;  
+
+        n2.i = 10;  
+        System.out.println(n2.i);  
+
+        n1.i = 20;  
+        System.out.println(n1.i);  
     }
 }
