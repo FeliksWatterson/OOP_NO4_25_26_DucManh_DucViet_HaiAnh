d7e9df58a0f4c1ea13bcf95a2173c4881c3f7d29
Le Thu Nguyen <nglthu1967@gmail.com>
2025-09-05 06:53:10 +0000

    constructor

--- DucManh_DucViet_HaiAnh_Project/src/App.java
+++ DucManh_DucViet_HaiAnh_Project/src/App.java
@@ -1,5 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        bia b1 = new bia();
+
+        bia b2 = new bia("Hanoi", "lap xanh", "bia");
+
+        bia b3 = new bia("Heniken xanh");
+
+        System.out.println("b1 = " + b1.nhanbia);
+        System.out.println("b2 = " + b2.nhanbia);//duy: Hanoi
+        System.out.println("b3 = " + b3.nhanbia + b3.lapbia);//duy: Hanoi
+
     }
 }
--- DucManh_DucViet_HaiAnh_Project/src/bia.java
+++ DucManh_DucViet_HaiAnh_Project/src/bia.java
@@ -1,21 +1,38 @@
-public class bia{
+public class bia {
 
-    //Bien thanh vien
+    // Bien thanh vien
 
     public String nhanbia;
     public String lapbia;
     public String chatlong;
 
-    //Phương thuc hoat dong: Methods: operation of object
+    // Phương thuc hoat dong: Methods: operation of object
 
-    public String layNhanBia(){
-          return nhanbia;
+    public String layNhanBia() {
+        return nhanbia;
     }
 
-    public String ganNhanBia(String nhan){
+    public String ganNhanBia(String nhan) {
         nhanbia = nhan;
 
         return nhanbia;
+    }
+
+    // khởi tạo
+    // constructor
+
+    bia() {
+    }
+
+    bia(String nhan, String lap, String chatLong) {
+        nhanbia = nhan;
+        lapbia = lap;
+        chatlong = chatLong;
+
+    }
+
+    bia(String lap) {
+        lapbia = lap;
     }
 
 }
