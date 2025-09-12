b7590491984f3adfc3e26c7e32b4324b42826bc7
Lê Hoàng Đức Mạnh <94680558+FeliksWatterson@users.noreply.github.com>
2025-09-12 08:31:32 +0000

    Add Composition

--- DucManh_DucViet_HaiAnh_Project/test/TestNNCollection.java
+++ DucManh_DucViet_HaiAnh_Project/test/TestNNCollection.java
@@ -1,22 +1,22 @@
 public class TestNNCollection {
     public static void main(String[] args) {
-        NNCollection danhBa = new NNCollection();
+        NNCollection phoneBook = new NNCollection();
 
-        danhBa.insert(new NameNumber("Nguyen", "0901-111-222"));
-        danhBa.insert(new NameNumber("Tran", "0902-222-333"));
-        danhBa.insert(new NameNumber("Le", "0903-333-444"));
+        phoneBook.insert(new NameNumber("Nguyen", "0901-111-222"));
+        phoneBook.insert(new NameNumber("Tran", "0902-222-333"));
+        phoneBook.insert(new NameNumber("Le", "0903-333-444"));
 
-        System.out.println("Danh ba:");
-        for (NameNumber nn : danhBa.getList()) {
+        System.out.println("Danh bạ:");
+        for (NameNumber nn : phoneBook.getList()) {
             System.out.println(nn);
         }
 
         String search = "Tran";
-        String number = danhBa.findNumber(search);
+        String number = phoneBook.findNumber(search);
         if (number != null) {
-            System.out.println("So cua " + search + ": " + number);
+            System.out.println("Số của " + search + ": " + number);
         } else {
-            System.out.println("Khong tim " + search);
+            System.out.println("Không tìm thấy " + search);
         }
     }
 }
