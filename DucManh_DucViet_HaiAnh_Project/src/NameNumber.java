8bbd65e4d5c6bfe227ecee01af95079472541f7b
Lê Hoàng Đức Mạnh <94680558+FeliksWatterson@users.noreply.github.com>
2025-09-12 07:33:55 +0000

    Add Composition

--- DucManh_DucViet_HaiAnh_Project/src/NameNumber.java
+++ DucManh_DucViet_HaiAnh_Project/src/NameNumber.java
@@ -1,17 +1,18 @@
 public class NameNumber {
     private String lastName;
-    private String phone;
+    private String Phone;
 
-    public NameNumber(String lastName, String phone) {
+    public NameNumber(String lastName, String Phone) {
         this.lastName = lastName;
-        this.phone = phone;
+        this.Phone = Phone;
     }
 
     public String getLastName() { return lastName; }
-    public String getPhone() { return phone; }
+    public String getPhone() { return Phone; }
 
     @Override
     public String toString() {
-        return lastName + " -> " + phone;
+        return lastName + " -> " + Phone;
     }
 }
+ 
--- DucManh_DucViet_HaiAnh_Project/test/TestNNCollection.java
+++ DucManh_DucViet_HaiAnh_Project/test/TestNNCollection.java
@@ -1,22 +1,22 @@
 public class TestNNCollection {
     public static void main(String[] args) {
-        NNCollection phoneBook = new NNCollection();
+        NNCollection danhBa = new NNCollection();
 
-        phoneBook.insert(new NameNumber("Nguyen", "0901-111-222"));
-        phoneBook.insert(new NameNumber("Tran", "0902-222-333"));
-        phoneBook.insert(new NameNumber("Le", "0903-333-444"));
+        danhBa.insert(new NameNumber("Nguyen", "0901-111-222"));
+        danhBa.insert(new NameNumber("Tran", "0902-222-333"));
+        danhBa.insert(new NameNumber("Le", "0903-333-444"));
 
         System.out.println("Danh ba:");
-        for (NameNumber nn : phoneBook.getList()) {
+        for (NameNumber nn : danhBa.getList()) {
             System.out.println(nn);
         }
 
         String search = "Tran";
-        String number = phoneBook.findNumber(search);
+        String number = danhBa.findNumber(search);
         if (number != null) {
             System.out.println("So cua " + search + ": " + number);
         } else {
-            System.out.println("Khong tim thay " + search);
+            System.out.println("Khong tim " + search);
         }
     }
 }
