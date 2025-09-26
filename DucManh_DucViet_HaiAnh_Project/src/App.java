be94be1bb5ad85cb4c1965489c4a127f0fb09db2
Lê Hoàng Đức Mạnh <94680558+FeliksWatterson@users.noreply.github.com>
2025-09-26 07:03:29 +0000

    Thuchanh6

--- DucManh_DucViet_HaiAnh_Project/src/App.java
+++ DucManh_DucViet_HaiAnh_Project/src/App.java
@@ -1,12 +1,15 @@
-public class App{
+public class App {
     public static void main(String[] args) {
-        System.out.println("TestUser");
+        System.out.println("TestUse:");
         TestUser.main(new String[]{});
 
-        System.out.println("TestTime");
+        System.out.println("TestTime:");
         TestTime.main(new String[]{});
 
-        System.out.println("TestRecursion");
+        System.out.println("TestRecursion:");
         TestRecursion.main(new String[]{});
+
+        System.out.println("TestSequence:");
+        TestSequence.main(new String[]{}); 
     }
 }
