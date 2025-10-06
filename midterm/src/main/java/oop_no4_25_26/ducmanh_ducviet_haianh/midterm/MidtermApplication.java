7dee02d37ec20640b8992ef19f7ae54c92e61c21
FeliksWatterson <ducmanhlehoang@gmail.com>
2025-10-06 16:28:19 +0700

    Change repo

---
+++ midterm/src/main/java/oop_no4_25_26/ducmanh_ducviet_haianh/midterm/MidtermApplication.java
@@ -0,0 +1,13 @@
+package oop_no4_25_26.ducmanh_ducviet_haianh.midterm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermApplication.class, args);
+	}
+
+}
---
+++ midterm/src/test/java/oop_no4_25_26/ducmanh_ducviet_haianh/midterm/MidtermApplicationTests.java
@@ -0,0 +1,13 @@
+package oop_no4_25_26.ducmanh_ducviet_haianh.midterm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MidtermApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
