e34033ad7045a84ac5de59434e7b7ada246858bd
Lê Hoàng Đức Mạnh <94680558+FeliksWatterson@users.noreply.github.com>
2025-10-10 06:52:34 +0000

    change thread

--- project/src/test/java/oop_no4_25_26/ducmanh_ducviet_haianh/midterm/threadExample.java
+++ project/src/test/java/oop_no4_25_26/ducmanh_ducviet_haianh/midterm/threadExample.java
@@ -2,17 +2,16 @@ package oop_no4_25_26.ducmanh_ducviet_haianh.midterm;
 
 public class threadExample extends Thread{
 public static int count = 0 ;
-public static void main(String[] args) {
-threadExample threadExp = new threadExample();
-threadExp.start();
-while (threadExp.isAlive()){
-System.out.println("running...");
-}
-System.out.println("count "+ count); //1
-count ++; //2
-System.out.println("count after ++"+count); //2
-}
-public void run(){//overridden run method of thread
-count ++; //1
-}
-}
+    public static void main(String[] args) {
+        threadExample threadExp = new threadExample();
+        threadExp.start();
+        System.out.println("count "+ count); //0
+        count ++; //2
+        System.out.println("In gia tri count: "+count); //2
+        System.out.println("In gia tri count dong so 11: "+count);
+    }
+
+        public void run(){//overridden run method of thread
+        count ++; //1
+        }
+} 
