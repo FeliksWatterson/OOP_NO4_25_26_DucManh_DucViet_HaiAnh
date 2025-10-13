07bc695493f2aa94aa97c66c65eacf009c9ef8d6
Lê Hoàng Đức Mạnh <94680558+FeliksWatterson@users.noreply.github.com>
2025-10-13 11:16:39 +0000

    Rearrange and fixed

---
+++ project/complete/src/main/java/oop_no4_25_26/ducmanh_ducviet_haianh/code/component/ReadingFromFile.java
@@ -0,0 +1,16 @@
+package oop_no4_25_26.ducmanh_ducviet_haianh.code.component;
+
+import java.io.File;
+import java.io.FileInputStream;
+public class fileReader {
+public static void main(String[] args) {
+File f = new File("final/src/test.txt");
+try {
+FileInputStream fis = new FileInputStream(f);
+int c;
+while ((c = fis.read()) != -1) {
+System.out.println((char) c);
+}
+} catch (Exception e) {
+e.printStackTrace();
+}} }
