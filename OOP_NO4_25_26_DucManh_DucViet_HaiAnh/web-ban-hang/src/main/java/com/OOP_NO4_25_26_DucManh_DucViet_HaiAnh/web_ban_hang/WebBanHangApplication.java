1225bcc27771e2d05625a9ab1e4214ab6f856c78
FeliksWatterson <ducmanhlehoang@gmail.com>
2025-10-21 18:10:06 +0700

    Change repo

---
+++ OOP_NO4_25_26_DucManh_DucViet_HaiAnh/web-ban-hang/src/main/java/com/OOP_NO4_25_26_DucManh_DucViet_HaiAnh/web_ban_hang/WebBanHangApplication.java
@@ -0,0 +1,13 @@
+package com.OOP_NO4_25_26_DucManh_DucViet_HaiAnh.web_ban_hang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBanHangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBanHangApplication.class, args);
+	}
+
+}
---
+++ OOP_NO4_25_26_DucManh_DucViet_HaiAnh/web-ban-hang/src/test/java/com/OOP_NO4_25_26_DucManh_DucViet_HaiAnh/web_ban_hang/WebBanHangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OOP_NO4_25_26_DucManh_DucViet_HaiAnh.web_ban_hang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBanHangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
